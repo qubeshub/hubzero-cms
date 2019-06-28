b7ddd414bc1f541f395b4a704a6411f589e199ee
BharathR8 <bharathchandra.bcr@gmail.com>
2019-06-28 13:25:47 -0400

    Made changes to the codebase

---
+++ app/plugins/groups/publications/assets/js/resource_plugin.js
@@ -0,0 +1,47 @@
+$(document).ready(function() {
+
+  // Display active filter status
+
+  var $sortBy = $('.sort-option li'),
+      $viewOption = $('.view-option li'),
+      $imgOption = $('.img-option li');
+
+
+  $sortBy.on('click', function() {
+    $(this).addClass('active');
+    $sortBy.not(this).removeClass('active');
+  });
+
+  $viewOption.on('click', function() {
+    $(this).addClass('active');
+    $viewOption.not(this).removeClass('active');
+  });
+
+  $imgOption.on('click', function() {
+    $(this).addClass('active');
+    $imgOption.not(this).removeClass('active');
+  });
+
+  // Check if image exists in list view and make necessary adjustments for mobile
+
+  var $resourceImg = $('.resource-info-wrapper img'),
+      $infoWrapper = $('.resource-info-wrapper'),
+      $info = $('.resource-info');
+
+  $(window).on('resize', function() {
+    var $windowSize = $(window).width();
+
+    if ($infoWrapper.has('img') && $windowSize < 1101) {
+      console.log('detected!');
+      $infoWrapper.css({'flex-wrap': 'wrap'});
+      $resourceImg.css({'width': '100%'});
+      $info.css({'width': '100%'});
+    } else {
+      console.log('too big!');
+      $infoWrapper.css({'flex-wrap': 'nowrap'});
+      $resourceImg.css({'width': 'auto'});
+      $info.css({'width': 'auto'});
+    }
+
+  });
+});
