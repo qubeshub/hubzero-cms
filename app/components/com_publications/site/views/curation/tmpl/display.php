693d50c54f897ea50a9fc43c1930e76a40ea8a0d
M. Drew LaMar <mdlama@wm.edu>
2021-02-19 14:12:03 -0500

    [com_publications] Squashed commit of the following:
    commit 6a9202d346f6a97ce176f1394a5382cf449dbd48
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Mon Feb 15 17:25:25 2021 -0500
    
        Add option to correctly handle HTML tags
    
        (cherry picked from commit 719e75c64bdf9d0e63d145497b212c38b9c90147)
    
    commit 5b08e6a43c085a760c237e276940cd0282efab8d
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Mon Feb 15 17:24:47 2021 -0500
    
        Update abstract character limit
    
        (cherry picked from commit feb75fe58c04e3ca0e27bc20748a31c80ef77391)
    
    commit f736e5e0d495093f6ab69d922fd2b123fe56cb70
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Mon Feb 15 17:24:20 2021 -0500
    
        Update abstract processing logic
    
        (cherry picked from commit ed75100b45d7491c35c3d77ab67289ff80e79900)
    
    commit f08bd5b0b49104c2e966702fa8bdd1fe2c876add
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Mon Feb 15 17:23:48 2021 -0500
    
        Linting
    
        (cherry picked from commit 47d879838697fdf9fe32ab93d24a1794a17b45a8)
    
    commit 68f13f6416c6546f7d28672c6d876de7f8878221
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Mon Feb 15 17:23:03 2021 -0500
    
        Avoid escaping abstract if new pubs is enabled
    
        (cherry picked from commit 31a5446a9ea8ca5ec6fd32dcbac1ef8937f310ee)
    
    commit eed03d0edd7762694b74cae63fafe9182869afe7
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Mon Feb 15 17:21:42 2021 -0500
    
        Remove duplicate test
    
        (cherry picked from commit 45b0ec28b1e7a50ae1779332b55001a5569ea742)
    
    commit 39b7efa02c08b548a9fe5204c0bd9ff3196dab97
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Mon Feb 15 17:21:04 2021 -0500
    
        Stop displaying current user/today date as the last reviewed info for those pubs that have no review history
    
        (cherry picked from commit 47b36675e89660ab0e34531645bf336c340183bb)
    
    commit 4db34985427f4da26f280198bd7153b3ce22cbfd
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Mon Feb 15 17:17:12 2021 -0500
    
        Change publication README file name
    
        Avoid clobbering READMEs attached by users
    
        fixes: https://purr.purdue.edu/support/ticket/2158
        (cherry picked from commit 5d67fd7dc5ea77da9c20832769378b8d2ad283fb)
    
    commit cfc8cd05437916bca381abefeee8bbf8c0c28666
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Mon Feb 15 17:15:47 2021 -0500
    
        update copyright years
    
        (cherry picked from commit 4e39de96bd33d687d9c3f7bde5721dc366fe2681)
    
    commit 6e2439ebb97ce23133eb6c4ccc09a60720c987b2
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Mon Feb 15 17:13:58 2021 -0500
    
        update copyright/trademark ownership
    
        (cherry picked from commit 7a772265691cebdda2695389145abe67bd7bcd96)
    
    commit f504a8d5ae884a0ff20184cde780b44519cad265
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Mon Feb 15 17:08:17 2021 -0500
    
        remove per file hubzero author references
    
        (cherry picked from commit 52173c3d9446ce86057bc80f8626511feee1c937)
    
    commit ec9b40146d2a42fcca40ae00701b97bed0838307
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Mon Feb 15 17:06:23 2021 -0500
    
        Remove unnecessary code
    
        (cherry picked from commit 99aa910d2d9b2dd0c4e6c0666f0a926a901178c3)

--- app/components/com_publications/site/views/curation/tmpl/display.php
+++ app/components/com_publications/site/views/curation/tmpl/display.php
@@ -1,7 +1,7 @@
 <?php
 /**
  * @package    hubzero-cms
- * @copyright  Copyright 2005-2019 HUBzero Foundation, LLC.
+ * @copyright  Copyright (c) 2005-2020 The Regents of the University of California.
  * @license    http://opensource.org/licenses/MIT MIT
  */
 
@@ -103,12 +103,17 @@ $this->css()
 
 									if ($row->state == 7)
 									{
-										$reviewed  = strtolower(Lang::txt('COM_PUBLICATIONS_CURATION_REVIEWED')) . ' <span class="prominent">' . Date::of($row->reviewed)->toLocal('M d, Y') . '</span> ';
+										$reviewed = '';
 
-										$reviewer  = User::getInstance($row->reviewed_by);
-										$name = $reviewer->get('name');
-										$name = $name ?: Lang::txt('JUNKNOWN');
-										$reviewed .= $reviewer ? ' <span class="block">' . Lang::txt('COM_PUBLICATIONS_CURATION_BY', $name) . '</span>' : '';
+										if (!empty($row->reviewed_by))
+										{
+											$reviewed = strtolower(Lang::txt('COM_PUBLICATIONS_CURATION_REVIEWED')) . ' <span class="prominent">' . Date::of($row->reviewed)->toLocal('M d, Y') . '</span> ';
+
+											$reviewer = User::getInstance($row->reviewed_by);
+											$name = $reviewer->get('name');
+											$name = $name ?: Lang::txt('JUNKNOWN');
+											$reviewed .= $reviewer ? ' <span class="block">' . Lang::txt('COM_PUBLICATIONS_CURATION_BY', $name) . '</span>' : '';
+										}
 									}
 
 									$class = $row->state == 5 ? 'status-pending' : 'status-wip';
