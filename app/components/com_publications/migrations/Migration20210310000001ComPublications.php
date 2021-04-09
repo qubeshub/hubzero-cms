cfef47b3915809813a82379bf6fda9cd031fcc1f
M. Drew LaMar <mdlama@wm.edu>
2021-04-09 09:15:30 -0400

    [fix] Migration file fix
    Needed to fix clean vagrant install.

--- app/components/com_publications/migrations/Migration20210310000001ComPublications.php
+++ app/components/com_publications/migrations/Migration20210310000001ComPublications.php
@@ -23,15 +23,20 @@ class Migration20210310000001ComPublications extends Base
 	public function up()
 	{
 		$from_group = \Hubzero\User\Group::getInstance(self::$from_group_cn);
-		$from_members = $from_group->get('members');
-
 		$to_group = \Hubzero\User\Group::getInstance(self::$to_group_cn);
-		$to_members = $to_group->get('members');
 
-		$add_members = array_diff($from_members, $to_members); // Don't add members already in group
+		if ($from_group && $to_group) {
+			$this->log(sprintf('Adding members from group %s to group %s', self::$from_group_cn, self::$to_group_cn));
+			$from_members = $from_group->get('members');
+			$to_members = $to_group->get('members');
 
-		$to_group->add('members', $add_members);
-		$to_group->update();
+			$add_members = array_diff($from_members, $to_members); // Don't add members already in group
+
+			$to_group->add('members', $add_members);
+			$to_group->update();
+		} else {
+			$this->log(sprintf('Group %s or group %s does not exist!', self::$from_group_cn, self::$to_group_cn));
+		}
 	}
 
 	/**
@@ -40,9 +45,14 @@ class Migration20210310000001ComPublications extends Base
 	public function down()
 	{
 		$to_group = \Hubzero\User\Group::getInstance(self::$to_group_cn);
-		$remove_members = array_diff($to_group->get('members'), $to_group->get('managers')); // Don't remove managers
+		if ($to_group) {
+			$this->log(sprintf('Removing members from group %s', self::$to_group_cn));
+			$remove_members = array_diff($to_group->get('members'), $to_group->get('managers')); // Don't remove managers
 
-		$to_group->remove('members', $remove_members);
-		$to_group->update();
+			$to_group->remove('members', $remove_members);
+			$to_group->update();
+		} else {
+			$this->log(sprintf('Group %s does not exist!', self::$to_group_cn));
+		}
 	}
 }
