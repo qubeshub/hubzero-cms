da2f90af49a4e37129b68512eba4d244de54e4d4
M. Drew LaMar <mdlama@wm.edu>
2021-10-01 16:45:15 -0400

    [fix] Fixed watch publication feature
    - Unsubscribe link was broken
    - Watch plugin wasn't being called on submit (in controllers/curation.php)

--- app/components/com_publications/site/views/emails/tmpl/watch_html.php
+++ app/components/com_publications/site/views/emails/tmpl/watch_html.php
@@ -96,7 +96,7 @@ $link = rtrim($base, '/') . '/' . trim($this->url, '/');
 					<tbody>
 						<tr>
 							<td style="text-align: left; padding: 0 0.5em;" cellpadding="0" cellspacing="0" border="0">
-								<div style="line-height: 1.6em; margin: 1em 0; padding: 0; text-align: left;"><?php echo $comment; ?></div>
+								<div style="line-height: 1.6em; margin: 1em 0; padding: 0; text-align: left;"><?php echo $this->message; ?></div>
 							</td>
 						</tr>
 					</tbody>
