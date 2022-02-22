867fcc8745fd7c311ff877037911557244ce8383
M. Drew LaMar <mdlama@wm.edu>
2022-02-22 10:50:02 -0500

    Added com_login as subtree

---
+++ app/components/com_login/site/views/login/tmpl/default.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @package    hubzero-cms
+ * @copyright  Copyright (c) 2005-2020 The Regents of the University of California.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
+defined('_HZEXEC_') or die();
+
+if ($this->user->get('guest')):
+	// The user is not logged in.
+	echo $this->loadTemplate('login');
+else:
+	// The user is already logged in.
+	echo $this->loadTemplate('logout');
+endif;
