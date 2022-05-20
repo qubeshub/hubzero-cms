13f45f6ffa4db52424050cc1069dad118f565fe5
M. Drew LaMar <mdlama@wm.edu>
2022-05-20 10:18:30 -0400

    Added com_search as subtree

---
+++ app/components/com_search/helpers/errorMessageHelper.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * @package    hubzero-cms
+ * @copyright  Copyright (c) 2005-2020 The Regents of the University of California.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
+namespace Components\Search\Helpers;
+
+class ErrorMessageHelper
+{
+
+	protected $breaks;
+
+	public function __construct()
+	{
+		$this->breaks = '<br/><br/>';
+	}
+
+	public function generateErrorMessage($errors)
+	{
+		$errorList = implode($this->breaks . '• ', $errors);
+
+		$errorMessage = "• $errorList";
+
+		return rtrim($errorMessage, $this->breaks);
+	}
+
+}
---
+++ app/components/com_search/models/basic/result/assoc.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ * @package    hubzero-cms
+ * @copyright  Copyright (c) 2005-2020 The Regents of the University of California.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
+namespace Components\Search\Models\Basic\Result;
+
+use Components\Search\Models\Basic\Result as SearchResult;
+
+/**
+ * Associative result
+ */
+abstract class Assoc extends SearchResult
+{
+
+	/**
+	 * Is the result a scalar?
+	 *
+	 */
+	abstract public function is_scalar();
+}
