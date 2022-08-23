ca07596ac9426f11e51dff6829e7bac6c52e34c5
M. Drew LaMar <mdlama@wm.edu>
2022-08-23 07:54:43 -0400

    [fix] Removed recalculate button

--- app/components/com_tags/admin/views/focusareas/display.php
+++ app/components/com_tags/admin/views/focusareas/display.php
@@ -11,7 +11,6 @@ defined('_HZEXEC_') or die();
 $canDo = Components\Tags\Helpers\Permissions::getActions();
 
 Toolbar::title(Lang::txt('COM_TAGS') . ': FOCUS AREAS', 'focusareas');
-Toolbar::custom('calculate', 'refresh', '', 'COM_TAGS_RECALCULATE', false);
 if ($canDo->get('core.create'))
 {
 	Toolbar::addNew();
