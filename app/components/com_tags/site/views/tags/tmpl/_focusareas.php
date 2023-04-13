00858ddb75d3ba06a746f1131cf25d935a6da421
M. Drew LaMar <mdlama@wm.edu>
2023-04-13 11:22:28 -0400

    [fix/feat] Accordion immediate on click
    Also added link from pub alignments to browse page.

--- app/components/com_tags/site/views/tags/tmpl/_focusareas.php
+++ app/components/com_tags/site/views/tags/tmpl/_focusareas.php
@@ -17,7 +17,7 @@ switch ($this->stage) {
     break;
     case 'during':
         $html .= "<li class='fa'>";
-        $html .= "<label style='display: inline;'" . ($this->child->about ? " title='" . htmlentities($this->child->about) . "' class='tooltips'" : "") . ">" . $this->child->label . "</label>";
+        $html .= "<label style='display: inline;'" . ($this->child->about ? " title='" . htmlentities($this->child->about) . "' class='tooltips'" : "") . "><a href='" . Route::url('/com_publications/browse?fl=' . $this->child->id) . "'>" . $this->child->label . "</a></label>";
         $html .= $this->child->render('view', $this->props);
         $html .= "</li>";
     break;
