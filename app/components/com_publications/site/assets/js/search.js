12d34566d884d309362d0ac5eb03cdfc845d5ebf
M. Drew LaMar <mdlama@wm.edu>
2023-05-18 07:58:11 -0400

    [refactor] fix fancybox on submit page
    (cherry picked from commit 6f30936a5320e06b6b55d5beb96e2c1cfe3bffb0)

--- app/components/com_publications/site/assets/js/search.js
+++ app/components/com_publications/site/assets/js/search.js
@@ -3,58 +3,19 @@ $(document).ready(function() {
   	return this + (this.indexOf('?') == -1 ? '?' : '&') + 'no_html=1';
   };
 
-  var url = '/publications/browse';
-  var limit = $('#limit').val();
-  var queryParams = '?search=';
-  var sortbyParams = '&sortby=date';
-  var limitParams = `&limit=${limit}&limitstart=0`;
-  var container = $('#results-container');
-  var urlToFetch = null;
-
-  // Remove onchange function for pagination
-  // const removeOnchange = () => {
-  //   $('#limit').removeAttr('onchange');
-  // }
-
-  $('#resourcesform').on('submit', function(e) {
-    e.preventDefault();
-
-    var inputTerms = $('#entry-search-field').val();
-    var encodedTerms = encodeURIComponent(inputTerms).replace(/%20/g, '+');
-    urlToFetch = `${url}${queryParams}${encodedTerms}${sortbyParams}${limitParams}`;
-
-    if (container.length && inputTerms.length) {
-      container.load(urlToFetch + ' #results-container');
-      window.history.pushState({href: urlToFetch}, '', urlToFetch);
-
-      return urlToFetch;
-    }
+  // Submit Resource Btn
+	$('#submit-resource').fancybox({
+      type: 'ajax',
+      scrolling: true,
+      autoSize: false,
+      fitToView: true,
+      titleShow: false,
+      tpl: {
+        wrap:'<div class="fancybox-wrap"><div class="fancybox-skin"><div class="fancybox-outer"><div id="sbox-content" class="fancybox-inner"></div></div></div></div>'
+      },
+      beforeLoad: function() {
+        href = $(this).attr('href');
+        $(this).attr('href', href.nohtml());
+      }
   });
-
-  // $('body').on('change', '#limit', function() {
-  //   console.log('limit change detected');
-  //   var newLimit = $('#limit').val();
-  //   limitParams = `&limit=${newLimit}&limitstart=0`;
-  //
-  //   if (urlToFetch === null) {
-  //     urlToFetch = `${url}${queryParams}${sortbyParams}${limitParams}`;
-  //     container.load(urlToFetch + ' #results-container');
-  //   } else {
-  //     urlToFetch = urlToFetch.replace(/limit=.+&limitstart=0/, 'limit=' + newLimit + '&limitstart=0');
-  //     container.load(urlToFetch + ' #results-container');
-  //   }
-  // });
-
-  $('body').on('click', '.pagination li a', function(e) {
-    e.preventDefault();
-
-    var page = $(this).attr('href');
-
-    if (window.location.href.indexOf('browse') > -1) {
-      container.load(page + ' #results-container');
-    }  else {
-      $('#contrib-section').load(page + ' #contrib-section');
-    }
-  });
-
 });
