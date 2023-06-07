f0468216bddbbba13686daea8966b20e2fddcdf1
Jenny Kwan <jakwan33@gmail.com>
2023-06-07 16:11:37 -0600

    [fix] alignments within supergroups

--- app/components/com_tags/site/views/tags/tmpl/_focusareas.php
+++ app/components/com_tags/site/views/tags/tmpl/_focusareas.php
@@ -13,11 +13,11 @@ $html = '';
 switch ($this->stage) {
     case 'before':
         if (count($this->children)) {
-            $html .= '<ul class="fa">';
+            $html .= '<ul class="fa-">';
         }
     break;
     case 'during':
-        $html .= "<li class='fa'>";
+        $html .= "<li class='fa-'>";
         $html .= "<label style='display: inline;'" . ($this->child->about ? " title='" . htmlentities($this->child->about) . "' class='tooltips'" : "") . "><a href='" . Route::url($url . '?fl=' . $this->child->id) . "'>" . $this->child->label . "</a></label>";
         $html .= $this->child->render('view', $this->props);
         $html .= "</li>";
