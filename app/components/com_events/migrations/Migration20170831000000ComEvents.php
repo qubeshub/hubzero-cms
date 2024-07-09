5c247342b4499c5de38d04bfd9c0cfed65ac33cf
M. Drew LaMar <mdlama@wm.edu>
2024-07-09 13:25:14 -0400

    Added com_events as subtree

---
+++ app/components/com_events/migrations/Migration20170831000000ComEvents.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * @package    hubzero-cms
+ * @copyright  Copyright (c) 2005-2020 The Regents of the University of California.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
+use Hubzero\Content\Migration\Base;
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+/**
+ * Migration script for adding component entry for com_events
+ **/
+class Migration20170831000000ComEvents extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		$this->addComponentEntry('events');
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		$this->deleteComponentEntry('events');
+	}
+}
