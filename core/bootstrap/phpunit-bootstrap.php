652e8add246c23d3efdd9658e73ba3b9f961ec98
Nicholas J. Kisseberth <nkisseberth@ucsd.edu>
2025-01-10 23:27:25 -0800

    [muse] restore muse test functionality

---
+++ core/bootstrap/phpunit-bootstrap.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * @package    hubzero-cms
+ * @copyright  Copyright (c) 2005-2024 The Regents of the University of California.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
+(new Hubzero\Base\Application())->boot();
