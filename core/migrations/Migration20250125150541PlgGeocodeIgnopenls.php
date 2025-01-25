92b154ac828945f6debcc6e5499f86c97d00187c
Nicholas J. Kisseberth <nkisseberth@ucsd.edu>
2025-01-25 15:10:55 -0800

    [php >=8.2][geocode] remove geocode plugin ifnopenls (provider class gone)

---
+++ core/migrations/Migration20250125150541PlgGeocodeIgnopenls.php
@@ -0,0 +1,34 @@
+<?php
+/**
+ * @package    hubzero-cms
+ * @copyright  Copyright (c) 2005-2024 The Regents of the University of California.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
+use Hubzero\Content\Migration\Base;
+
+// No direct access
+defined('_HZEXEC_') or die();
+
+/**
+ * Migration script for removing Geocode - Ignopenls plugin
+ **/
+class Migration20250125150541PlgGeocodeIgnopenls extends Base
+{
+	/**
+	 * Up
+	 **/
+	public function up()
+	{
+		$this->deletePluginEntry('geocode', 'ignopenls');
+	}
+
+	/**
+	 * Down
+	 **/
+	public function down()
+	{
+		$this->addPluginEntry('geocode', 'ignopenls', 0);
+	}
+}
+
