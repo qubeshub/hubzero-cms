16d190632d6ddfaf9dd6e368a723002c42358e37
Nicholas J. Kisseberth <nkisseberth@ucsd.edu>
2025-02-24 16:03:52 -0800

    [lucent] add raw layout option to mod_custom module

---
+++ core/modules/mod_custom/tmpl/raw.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * @package    hubzero-cms
+ * @copyright  Copyright (c) 2005-2025 The Regents of the University of California.
+ * @license    http://opensource.org/licenses/MIT MIT
+ */
+
+// no direct access
+defined('_HZEXEC_') or die;
+?>
+<?php echo $module->content; ?>
