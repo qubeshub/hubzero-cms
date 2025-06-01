9bc16144701f58889d20cbf529787b1a31d1f99c
M. Drew LaMar <mdlama@wm.edu>
2025-06-01 11:41:39 -0400

    [plg_groups_resources] Squashed commit c6069e9525...dce78f8fe0
    commit 62abb10548c3d3265f9d024d50d5c351cec8c03e
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Mon May 26 17:53:19 2025 -0400
    
        use parse_ini_string|parse_ini_file INI_SCANNER_RAW for speed and security
    
        (cherry picked from commit c6069e9525898232c70fa90b2839a97e5ccc1798)
    
    commit ab86cd84c4800b8850c43628d2304341021b942f
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Mon May 26 17:52:25 2025 -0400
    
        pass null arguments as '' where needed
    
        (cherry picked from commit 804415e1c3262e0d55293df26c9aa8c771108124)
    
    commit 47128bfbb0bd9bc5e7fa90d52e1c74707162c5bc
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Mon May 26 17:51:27 2025 -0400
    
        [HEAL-16] add resource access control lists
    
        (cherry picked from commit 5cc9a4080453dbee8add070e4ed9048114e86eee)
    
    commit 060da1b5dfca0ce7fac45c01b212512ba59b9910
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Mon May 26 17:50:13 2025 -0400
    
        [PHP80] Required parameter follows optional parameter
    
        (cherry picked from commit 0018c5224b2312a2e7c1a707bc309e7bf476f89d)
    
    commit 0a64852b90b7b33064df9945513dc0cf999892a1
    Author: M. Drew LaMar <mdlama@wm.edu>
    Date:   Mon May 26 17:49:28 2025 -0400
    
        fix: Add ARIA labels to nav landmarks
    
        Refs: https://dequeuniversity.com/rules/axe/4.4/landmark-unique
        (cherry picked from commit dce78f8fe07c2dd05a836e356510263449d036e9)

--- app/plugins/groups/resources/views/results/tmpl/default.php
+++ app/plugins/groups/resources/views/results/tmpl/default.php
@@ -88,7 +88,7 @@ if ($this->cats)
 		<input type="hidden" name="area" value="<?php echo $this->escape($this->active); ?>" />
 
 		<div class="container">
-			<nav class="entries-filters">
+			<nav class="entries-filters" aria-label="<?php echo Lang::txt('JGLOBAL_FILTER_AND_SORT_RESULTS'); ?>">
 				<ul class="entries-menu filter-options">
 					<?php if (count($links) > 0) { ?>
 						<li class="filter-categories">
@@ -118,6 +118,11 @@ if ($this->cats)
 							<?php echo Lang::txt('PLG_GROUPS_RESOURCES_ACCESS_PRIVATE'); ?>
 						</a>
 					</li>
+                                        <li>
+                                                <a<?php echo ($this->access == 'shared') ? ' class="active"' : ''; ?> href="<?php echo Route::url('index.php?option=' . $this->option . '&cn=' . $this->group->get('cn') . '&active=resources&area=' . urlencode(stripslashes($this->active)) . '&sort=' . $this->sort . '&access=shared&limit=' . $this->limit); ?>">
+                                                        <?php echo Lang::txt('PLG_GROUPS_RESOURCES_ACCESS_SHARED'); ?>
+                                                </a>
+                                        </li>
 				</ul>
 
 				<ul class="entries-menu">
